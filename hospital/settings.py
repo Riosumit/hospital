eaa7cad0b1e0d70579b9b60ea96be79349006b43
brainbuzz1 <rajsumit22032003@gmail.com>
2024-03-12 01:05:42 +0530

    deployment ready

--- hospital/settings.py
+++ hospital/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$p2at_j#1$)=xi2eaohvgfe$koxcqvw2!hwrzzo4u&dk=dhowq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
