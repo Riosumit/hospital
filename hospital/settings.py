d38d29a58eaa052c19cdc647752c2a5cd6c9bdac
brainbuzz1 <rajsumit22032003@gmail.com>
2024-03-21 12:30:10 +0530

    blog post added

--- hospital/settings.py
+++ hospital/settings.py
@@ -130,9 +130,8 @@ if DEBUG:
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'staticfiles')
+        os.path.join(BASE_DIR, 'static')
     ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
