ef8e924d02e27e6d6a4d045d5ed91010855f6a67
brainbuzz1 <rajsumit22032003@gmail.com>
2024-03-21 12:34:07 +0530

    blog post added

--- hospital/settings.py
+++ hospital/settings.py
@@ -124,15 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "staticfiles",
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
